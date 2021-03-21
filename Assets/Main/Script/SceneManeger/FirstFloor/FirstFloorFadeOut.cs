b8160d9b6e7c81983a58c2d15d1da10cc56899b1
yanbarasuka <shiryu0654@outlook.jp>
2021-03-21 12:44:25 +0900

    一階層
    90％くらい完成。あとはブラッシュアップの際に付け足す

---
+++ Assets/Main/Script/SceneManeger/FirstFloor/FirstFloorFadeOut.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FirstFloorFadeOut : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+
+    public bool fadeStart = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.DOFade(0, 0);
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeStart == true)
+        {
+            fadeOut();
+        }
+    }
+    void fadeOut()
+    {
+        canvasGroup.DOFade(1, 3f);
+    }
+}
